12e702b245576c24f452f3886010dabb8d67a251
dipenbharati <dipendrab226@gmail.com>
2026-01-26 12:40:37 -0800

    Week3 second lab

---
+++ Week3/second_lab/README.md
@@ -0,0 +1,4 @@
+# Environment setup
+- Create the virtual env - `python3 -m venv env`
+- Activate the virtual env - `source env/bin/activate`
+- Install the requirements - `pip3 install -r requirements.txt`
---
+++ Week3/second_lab/config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import os
+
+""" Bot Configuration """
+
+
+class DefaultConfig:
+    """ Bot Configuration """
+
+    PORT = 3978
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_TYPE = os.environ.get("MicrosoftAppType", "MultiTenant")
+    APP_TENANTID = os.environ.get("MicrosoftAppTenantId", "")
+
+    # Microsoft AI Language Service Engdoint
+    ENDPOINT_URI = os.environ.get("MicrosoftAIServiceEndpoint","")
+    # print(f"Endpoint URI = {ENDPOINT_URI}")
+    API_KEY = os.environ.get("MicrosoftAPIKey","")
